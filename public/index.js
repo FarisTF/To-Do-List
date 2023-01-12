09b7847a646e8d9b117b95907d4b0b467ae9136a
FarisTF <faris.tristan@gmail.com>
2023-01-12 22:00:59 +0700

    redeploy

--- public/index.js
+++ public/index.js
@@ -8,133 +8,141 @@ const removeButton = document.querySelector(".remove-btn");
 
 let id, kegiatan, done;
 
-fetch("https://todolistfaris.herokuapp.com/api")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    data.forEach(function (post) {
-      if (post.done == false) {
-        const todoDiv = document.createElement("div");
-        todoDiv.classList.add("todo");
+fetch("https://to-do-list-production-4e8b.up.railway.app/api")
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        data.forEach(function (post) {
+            if (post.done == false) {
+                const todoDiv = document.createElement("div");
+                todoDiv.classList.add("todo");
 
-        const todo = document.createElement("li");
-        todo.id = String(post._id);
-        todo.textContent = post.kegiatan;
-        todoDiv.appendChild(todo);
+                const todo = document.createElement("li");
+                todo.id = String(post._id);
+                todo.textContent = post.kegiatan;
+                todoDiv.appendChild(todo);
 
-        const deleteButton = document.createElement("button");
-        deleteButton.classList.add("complete-btn");
-        deleteButton.textContent = "V";
-        todoDiv.appendChild(deleteButton);
+                const deleteButton = document.createElement("button");
+                deleteButton.classList.add("complete-btn");
+                deleteButton.textContent = "V";
+                todoDiv.appendChild(deleteButton);
 
-        todoList.appendChild(todoDiv);
+                todoList.appendChild(todoDiv);
 
-        todoInput.value = "";
-      } else {
-        const todoDiv = document.createElement("div");
-        todoDiv.classList.add("todo");
+                todoInput.value = "";
+            } else {
+                const todoDiv = document.createElement("div");
+                todoDiv.classList.add("todo");
 
-        const todo = document.createElement("li");
-        todo.id = String(post._id);
-        todo.textContent = post.kegiatan;
-        todoDiv.appendChild(todo);
+                const todo = document.createElement("li");
+                todo.id = String(post._id);
+                todo.textContent = post.kegiatan;
+                todoDiv.appendChild(todo);
 
-        const removeButton = document.createElement("button");
-        removeButton.classList.add("remove-btn");
-        removeButton.textContent = "X";
-        todoDiv.appendChild(removeButton);
+                const removeButton = document.createElement("button");
+                removeButton.classList.add("remove-btn");
+                removeButton.textContent = "X";
+                todoDiv.appendChild(removeButton);
 
-        document.querySelector(".com-list").appendChild(todoDiv);
-      }
+                document.querySelector(".com-list").appendChild(todoDiv);
+            }
+        });
     });
-  });
 
 function addTodo(e) {
-  e.preventDefault();
-  if (!(todoInput.value === "")) {
-    // post request
-    fetch("https://todolistfaris.herokuapp.com/api", {
-      method: "POST",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ kegiatan: todoInput.value, done: false }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
+    e.preventDefault();
+    if (!(todoInput.value === "")) {
+        // post request
+        fetch("https://to-do-list-production-4e8b.up.railway.app/api", {
+            method: "POST",
+            headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({ kegiatan: todoInput.value, done: false }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
 
-        // ngubah dom
-        const todoDiv = document.createElement("div");
-        todoDiv.classList.add("todo");
+                // ngubah dom
+                const todoDiv = document.createElement("div");
+                todoDiv.classList.add("todo");
 
-        const todo = document.createElement("li");
-        todo.id = String(data._id);
-        todo.textContent = todoInput.value;
-        todoDiv.appendChild(todo);
+                const todo = document.createElement("li");
+                todo.id = String(data._id);
+                todo.textContent = todoInput.value;
+                todoDiv.appendChild(todo);
 
-        const deleteButton = document.createElement("button");
-        deleteButton.classList.add("complete-btn");
-        deleteButton.textContent = "V";
-        todoDiv.appendChild(deleteButton);
+                const deleteButton = document.createElement("button");
+                deleteButton.classList.add("complete-btn");
+                deleteButton.textContent = "V";
+                todoDiv.appendChild(deleteButton);
 
-        todoList.appendChild(todoDiv);
+                todoList.appendChild(todoDiv);
 
-        todoInput.value = "";
-      });
-  }
+                todoInput.value = "";
+            });
+    }
 }
 
 function completeTodo(e) {
-  // ngubah dom dlu
-  const item = e.target;
-  if (item.classList[0] === "complete-btn") {
-    const buangDB = item.parentElement.querySelector("li");
-    const parent = item.parentElement;
-    const removeButton = document.createElement("button");
-    removeButton.classList.add("remove-btn");
-    removeButton.textContent = "X";
-    parent.appendChild(removeButton);
+    // ngubah dom dlu
+    const item = e.target;
+    if (item.classList[0] === "complete-btn") {
+        const buangDB = item.parentElement.querySelector("li");
+        const parent = item.parentElement;
+        const removeButton = document.createElement("button");
+        removeButton.classList.add("remove-btn");
+        removeButton.textContent = "X";
+        parent.appendChild(removeButton);
 
-    document.querySelector(".com-list").appendChild(parent);
-    item.remove();
+        document.querySelector(".com-list").appendChild(parent);
+        item.remove();
 
-    // patch request
-    fetch("https://todolistfaris.herokuapp.com/api/" + buangDB.id, {
-      method: "PATCH",
-      body: JSON.stringify({
-        kegiatan: buangDB.textContent,
-        done: true,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-      });
-  }
+        // patch request
+        fetch(
+            "https://to-do-list-production-4e8b.up.railway.app/api/" +
+                buangDB.id,
+            {
+                method: "PATCH",
+                body: JSON.stringify({
+                    kegiatan: buangDB.textContent,
+                    done: true,
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8",
+                },
+            }
+        )
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+            });
+    }
 }
 
 function removeTodo(e) {
-  const item = e.target;
-  if (item.classList[0] === "remove-btn") {
-    const buangDB = item.parentElement.querySelector("li");
+    const item = e.target;
+    if (item.classList[0] === "remove-btn") {
+        const buangDB = item.parentElement.querySelector("li");
 
-    // delete request
-    fetch("https://todolistfaris.herokuapp.com/api/" + buangDB.id, {
-      method: "DELETE",
-    })
-      .then((response) => response)
-      .then((HTML) => {
-        console.log(HTML);
-      });
+        // delete request
+        fetch(
+            "https://to-do-list-production-4e8b.up.railway.app/api/" +
+                buangDB.id,
+            {
+                method: "DELETE",
+            }
+        )
+            .then((response) => response)
+            .then((HTML) => {
+                console.log(HTML);
+            });
 
-    const parent = item.parentElement;
-    parent.remove();
-  }
+        const parent = item.parentElement;
+        parent.remove();
+    }
 }
 
 todoButton.addEventListener("click", addTodo);
